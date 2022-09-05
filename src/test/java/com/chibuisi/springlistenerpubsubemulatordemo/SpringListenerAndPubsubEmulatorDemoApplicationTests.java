e977c3147dea8e599a14640dea52a1f78f31aff8
chibuisi <amiaka.chibuisi@gmail.com>
2022-09-05 09:00:42 -0700

    created channel and activator

---
+++ src/test/java/com/chibuisi/springlistenerpubsubemulatordemo/SpringListenerAndPubsubEmulatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chibuisi.springlistenerpubsubemulatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringListenerAndPubsubEmulatorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
